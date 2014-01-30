1c1dab1333cb9d18947eaf998e15a427fc2c26f9
Andrew Kofink <ajkofink@gmail.com>
2014-01-30 01:23:18 -0500

    Add basic acceslerometer example

--- brushless_dc_esc/brushless_dc_esc.ino
+++ brushless_dc_esc/brushless_dc_esc.ino
@@ -1,68 +1,61 @@
 int out1 = 3;
-int enable1 = 5;
-int out2 = 6;
-int enable2 = 9;
-int out3 = 10;
-int enable3 = 11;
+int out2 = 5;
+int out3 = 6;
+int out4 = 9;
+int out5 = 10;
+int out6 = 11;
 
 int test = 13;
-
 int test_val = 0;
+
+int duty = 255;
 
 int read_val = 0;
 float read_frequency; // Calculated from read_val
 float decimal_position;
 
 float frequency = 1; // Default value of 1Hz
-float points_per_period = 12; // Data Points per Period
-float offset;
-float period;
-
-int step_count = 0;
-
-float x; // Our independant variable
 
 void setup() {
   pinMode(out1, OUTPUT);
   pinMode(out2, OUTPUT);
   pinMode(out3, OUTPUT);
-  pinMode(enable1, OUTPUT);
-  pinMode(enable2, OUTPUT);
-  pinMode(enable3, OUTPUT);
+  pinMode(out4, OUTPUT);
+  pinMode(out5, OUTPUT);
+  pinMode(out6, OUTPUT);
   pinMode(test, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   handleSerial();
+  digitalWrite(test, test_val++ % 2);
   
-  period = 1.0 / frequency;
-  
-  write(255, 255, 0, 255, 0, 0);
+  write(duty, 0, 0, duty, 0, duty);
   d();
-  write(255, 255, 0, 0, 0, 255);
+  write(duty, 0, 0, duty, 0, duty);
   d();
-  write(0, 0, 255, 255, 0, 255);
+  write(0, duty, duty, 0, 0, duty);
   d();
-  write(0, 255, 255, 255, 0, 0);
+  write(0, duty, duty, 0, 0, duty);
   d();
-  write(0, 255, 0, 0, 255, 255);
+  write(0, duty, 0, duty, duty, 0);
   d();
-  write(0, 0, 0, 255, 255, 255);
+  write(0, duty, 0, duty, duty, 0);
   d();
 }
 
-void write(float x, float ex, float y, float ey, float z, float ez) {
-  analogWrite(out1, x);
-  analogWrite(out2, y);
-  analogWrite(out3, z);
-  analogWrite(enable1, ex);
-  analogWrite(enable2, ey);
-  analogWrite(enable3, ez);
+void write(float a, float b, float c, float d, float e, float f) {
+  analogWrite(out1, a);
+  analogWrite(out2, b);
+  analogWrite(out3, c);
+  analogWrite(out4, d);
+  analogWrite(out5, e);
+  analogWrite(out6, f);
 }
 
 void d() {
-  delay(1000.0 * period / 6.0);
+  delay(1000.0 * 1.0 / (6.0 * frequency));
 }
 
 void handleSerial() {
@@ -77,7 +70,7 @@ void handleSerial() {
         decimal_position = 0.1;
       } else if (read_val == 27 && Serial.read() == 91) { // Arrow
         read_val = Serial.read();
-        if (read_val == 66) { // Down
+        if (read_val == 66 && frequency > 1) { // Down
           frequency--;
         } else if (read_val == 65) { // Up
           frequency++;
